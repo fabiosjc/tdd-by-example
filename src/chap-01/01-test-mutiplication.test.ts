4b6eb38ca06de09eb64e9181e2b7b7c36138fbbd
Fabio Henrique <997525+fabiosjc@users.noreply.github.com>
2022-04-29 17:09:42 -0300

    testMultiplication red

--- src/chap-01/01-test-mutiplication.test.ts
+++ src/chap-01/01-test-mutiplication.test.ts
@@ -1,5 +1,7 @@
+import { Dollar } from "./Dollar"
+
 it('testMultiplication', () => {
-  Dollar five = new Dollar(3)
+  const five = new Dollar(3)
   five.times(2)
-  assertEquals(10, five.amount)
+  expect(five.amount).toBe(10)
 })
---
+++ src/chap-01/Dollar.ts
@@ -0,0 +1,12 @@
+export class Dollar {
+  amount: number
+
+  constructor(amount: number) {  
+  }
+
+  times(multiplier: number): void {
+
+  }
+}
+
+// export Dollar
