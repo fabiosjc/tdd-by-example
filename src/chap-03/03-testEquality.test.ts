e9a474a574020bfc70ddd289ce57580501a158a1
Fábio Henrique <997525+fabiosjc@users.noreply.github.com>
2022-05-16 11:15:00 -0300

    [chap-03]  Test of Equality RED

--- src/chap-03/03-testEquality.test.ts
+++ src/chap-03/03-testEquality.test.ts
@@ -1,5 +1,6 @@
 import { Dollar } from "./Dollar"
 
 it('testEquality', () => {
-  expect(new Dollar(5).equals(new Dollar(5)))
+  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+  expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
 })
