90b0dc1eb3a1ef5d41f36266b7a272d0f329405a
Fábio Henrique <997525+fabiosjc@users.noreply.github.com>
2022-05-18 11:07:01 -0300

    [chap-07] Comparing Franc with Dollars

---
+++ src/chap-07/07-equality.test.ts
@@ -0,0 +1,10 @@
+import { Dollar } from "./dollar"
+import { Franc } from "./franc"
+
+it('testEquality', () => {
+  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+  expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+
+  expect(new Franc(5).equals(new Franc(5))).toBeTruthy()
+  expect(new Franc(5).equals(new Franc(6))).toBeFalsy()
+})
---
+++ src/chap-07/dollar.ts
@@ -0,0 +1,13 @@
+import { Money } from "./money"
+
+export class Dollar extends Money {
+  constructor(amount: number) { 
+    super()
+    this.amount = amount
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier)
+  }
+
+}
